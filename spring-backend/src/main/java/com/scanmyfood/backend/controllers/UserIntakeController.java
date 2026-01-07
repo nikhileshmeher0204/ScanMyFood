d91ef146cbc8d26f6e325f83b0a7eb733f7c8d3f
Nikhilesh <nikhileshmeher24@gmail.com>
2026-01-07 13:25:38 +0530

    added getIntake API

--- spring-backend/src/main/java/com/scanmyfood/backend/controllers/UserIntakeController.java
+++ spring-backend/src/main/java/com/scanmyfood/backend/controllers/UserIntakeController.java
@@ -2,6 +2,7 @@ package com.scanmyfood.backend.controllers;
 
 import com.scanmyfood.backend.models.ApiResponse;
 import com.scanmyfood.backend.models.SaveScannedFoodInput;
+import com.scanmyfood.backend.models.UserIntakeOutput;
 import com.scanmyfood.backend.services.UserIntakeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.time.LocalDate;
+
 
 @Slf4j
 @RestController
-@RequestMapping("/api/user/save")
+@RequestMapping("/api/user")
 public class UserIntakeController {
 
     @Autowired
@@ -35,5 +38,19 @@ public class UserIntakeController {
 
 
         return ResponseEntity.ok(ApiResponse.success(null, "Scanned food intake saved successfully."));
+    }
+
+    @GetMapping("/intake")
+    public ResponseEntity<ApiResponse<UserIntakeOutput>> getDailyIntake(
+            @RequestParam("userId") String userId,
+            @RequestParam("date") LocalDate date
+    ) throws Exception {
+        log.info("Fetching daily intake for userId: {} on date: {}", userId, date);
+        UserIntakeOutput dailyIntake = userIntakeService.getUserIntake(userId, date);
+        if(dailyIntake.getFoodAnalysisResponse().isEmpty()){
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(ApiResponse.success(dailyIntake, "Daily intake fetched successfully."));
+
     }
 }
