f0b6a9afb7c7ce4af98e56e30d7774aefecca240
Nikhilesh <nikhileshmeher24@gmail.com>
2026-01-11 00:41:53 +0530

    Changes for Daily Intake View - FoodHistoryCard

--- flutter-app/lib/views/widgets/food_history_card.dart
+++ flutter-app/lib/views/widgets/food_history_card.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:read_the_label/models/daily_intake_record.dart';
 import 'package:read_the_label/theme/app_colors.dart';
 import 'package:read_the_label/viewmodels/daily_intake_view_model.dart';
-import 'package:read_the_label/viewmodels/ui_view_model.dart';
-import 'package:read_the_label/views/screens/meal_description_analysis_view.dart';
+import 'package:read_the_label/views/widgets/add_intake_desc_button.dart';
 import 'package:read_the_label/views/widgets/food_history_item_card.dart';
-import 'package:read_the_label/views/widgets/food_input_form.dart';
 import '../../theme/app_text_styles.dart';
 
 class FoodHistoryCard extends StatelessWidget {
@@ -42,10 +39,10 @@ class FoodHistoryCard extends StatelessWidget {
                   builder: (context, viewModel, child) {
                 final List<DailyIntakeRecord> todayItems = viewModel
                     .userIntakeOutput!.dailyIntake
-                    .where((item) => isSameDay(item.createdAt, selectedDate))
+                    .where((item) =>
+                        viewModel.isSameDay(item.createdAt, selectedDate))
                     .toList()
-                  ..sort((a, b) =>
-                      a.createdAt!.compareTo(b.createdAt!)); // Sort by time
+                  ..sort((a, b) => a.createdAt!.compareTo(b.createdAt!));
 
                 return ListView.builder(
                   padding: const EdgeInsets.only(top: 5),
@@ -80,86 +77,11 @@ class FoodHistoryCard extends StatelessWidget {
                   },
                 );
               }),
-              GestureDetector(
-                onTap: () {
-                  showCupertinoSheet<void>(
-                    context: context,
-                    enableDrag: true,
-                    builder: (context) => Material(
-                      child: FoodInputForm(
-                        onSubmit: () {
-                          Navigator.push(
-                            context,
-                            CupertinoPageRoute(
-                              builder: (context) => Consumer<UiViewModel>(
-                                builder: (context, uiProvider, _) =>
-                                    const MealDescriptionAnalysisView(),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  );
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      colors: [
-                        Color.fromARGB(255, 237, 202, 149),
-                        Color.fromARGB(255, 253, 142, 81),
-                        Color.fromARGB(255, 255, 0, 85),
-                        Color.fromARGB(255, 0, 21, 255),
-                      ],
-                      stops: [0.2, 0.4, 0.6, 1.0],
-                      begin: Alignment.topLeft,
-                      end: Alignment.bottomRight,
-                    ),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    spacing: 5,
-                    children: [
-                      const Icon(
-                        Icons.auto_awesome,
-                        color: Color.fromARGB(255, 0, 21, 255),
-                        size: 20,
-                      ),
-                      Text(
-                        "Add intake via text description",
-                        style: AppTextStyles.bodyLarge.copyWith(
-                          fontWeight: FontWeight.w500,
-                          foreground: Paint()
-                            ..shader = const LinearGradient(
-                              colors: <Color>[
-                                Color.fromARGB(255, 0, 21, 255),
-                                Color.fromARGB(255, 255, 0, 85),
-                                Color.fromARGB(255, 250, 220, 194),
-                              ],
-                              stops: [0.3, 0.5, 0.8],
-                              begin: Alignment.centerLeft,
-                              end: Alignment.centerRight,
-                            ).createShader(
-                              const Rect.fromLTWH(0.0, 0.0, 250.0, 16.0),
-                            ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              const AddIntakeDescButton()
             ],
           ),
         ],
       ),
     );
-  }
-
-  bool isSameDay(DateTime? date1, DateTime date2) {
-    if (date1 == null) return false;
-    return date1.year == date2.year &&
-        date1.month == date2.month &&
-        date1.day == date2.day;
   }
 }
